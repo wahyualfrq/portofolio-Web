7920d71b4921f44b489e28eebb41db3eff79aa14
wahyualfrq <wahyudialfurqon109@gmail.com>
2026-06-20 18:44:00 +0700

    deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -395,7 +395,7 @@ export default function HomePage() {
             <div className="relative h-full aspect-square max-w-full flex items-end justify-center p-6 md:p-8 lg:p-12">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
-                src="/images/fix.png"
+                src="/images/fix2.png"
                 alt="Wahyudi Alfurqon"
                 className="w-full h-full object-contain object-bottom"
               />
